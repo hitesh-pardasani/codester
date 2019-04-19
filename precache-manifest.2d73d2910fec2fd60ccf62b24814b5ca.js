80b89492c761ac188f5e6c936bf47d8ec6f8b375
PARVA <parvapatel12@gmail.com>
2019-04-19 11:35:19 +0530

    Updates

---
+++ precache-manifest.2d73d2910fec2fd60ccf62b24814b5ca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "924d72d26abb1992b47108d6c704271d",
+    "url": "/codester/static/media/blue.924d72d2.jpg"
+  },
+  {
+    "revision": "db16ece7d8b8b670f1ae",
+    "url": "/codester/static/css/main.ea590e78.chunk.css"
+  },
+  {
+    "revision": "7242bca5a90011610093",
+    "url": "/codester/static/js/runtime~main.b4b94950.js"
+  },
+  {
+    "revision": "40f4448069d1e09a33455de772c62dfd",
+    "url": "/codester/static/media/deep.40f44480.jpg"
+  },
+  {
+    "revision": "db13a83bf358188ef47c",
+    "url": "/codester/static/js/2.cd26cc8c.chunk.js"
+  },
+  {
+    "revision": "13a2e8088eb09d1f479ef1ae3b49760f",
+    "url": "/codester/static/media/avatar-big-01.13a2e808.jpg"
+  },
+  {
+    "revision": "db16ece7d8b8b670f1ae",
+    "url": "/codester/static/js/main.3b529260.chunk.js"
+  },
+  {
+    "revision": "e8b69bc5467e962bb170a13f09c2c01f",
+    "url": "/codester/static/media/upvote.e8b69bc5.png"
+  },
+  {
+    "revision": "783e5ad33cc6906536f8fbbb8a1b31ac",
+    "url": "/codester/static/media/upvote_bw.783e5ad3.png"
+  },
+  {
+    "revision": "536be29800b92a3e314e660952d9ab33",
+    "url": "/codester/static/media/logo.536be298.png"
+  },
+  {
+    "revision": "db13a83bf358188ef47c",
+    "url": "/codester/static/css/2.b6cb871d.chunk.css"
+  },
+  {
+    "revision": "321f5f4f9157f8b9419972e2af4d3d05",
+    "url": "/codester/index.html"
+  }
+];
